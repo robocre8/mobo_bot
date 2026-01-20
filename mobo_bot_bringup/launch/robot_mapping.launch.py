d05980e6a9eb09248e65e2039134ef8ae6b12091
Obiagba Samuel <samuel.c.agba@gmail.com>
2026-01-20 01:11:22 +0100

    Disable camera usage in robot mapping launch

--- mobo_bot_bringup/launch/robot_mapping.launch.py
+++ mobo_bot_bringup/launch/robot_mapping.launch.py
@@ -77,7 +77,7 @@ def generate_launch_description():
               'use_sim_time': 'False',
               'use_ekf': use_ekf,
               'use_lidar': 'True',
-              'use_camera': 'True',
+              'use_camera': 'False',
             }.items(),
   )
 
